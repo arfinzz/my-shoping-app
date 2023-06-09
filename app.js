920b970ad316256fae3ac50dcd656c90f0c3dd58
arfinzz <khanarfin2000@gmail.com>
2023-06-09 14:11:27 +0530

    Connected the app to mysql database

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express=require('express');
 const path=require('path');
 const bodyParser=require('body-parser');
 
+const db=require('./utils/database');
+
 
 const adminRoutes=require('./routes/admin');
 const shopRoutes=require('./routes/shop');
